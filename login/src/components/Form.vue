c7aac85ac9413c5edd42cc52ee8c6f60f6bbfbbf
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-12-07 17:17:55 +0800

    snackbar

--- login/src/components/Form.vue
+++ login/src/components/Form.vue
@@ -1,5 +1,11 @@
 <template>
 <div id="background">
+    <v-snackbar v-model="snackbar" :timeout="timeout" :top="y === 'top'">
+        {{ text }}
+        <v-btn color="blue" text @click="snackbar = false">
+            Close
+        </v-btn>
+    </v-snackbar>
     <v-flex justify-center>
         <h2 class="subtitle-110 text-center">Employ</h2>
 
@@ -10,7 +16,7 @@
                 </center>
                 <form id="form">
                     <v-container>
-                        <v-text-field  prepend-inner-icon="people" v-model="username" :rules="[rules.required]" label="Username"></v-text-field>
+                        <v-text-field prepend-inner-icon="people" v-model="username" :rules="[rules.required]" label="Username"></v-text-field>
                         <v-text-field prepend-inner-icon="lock" v-model="password" :append-icon="show1 ? 'visibility' : 'visibility_off'" :type="show1 ? 'text' : 'password'" name="input-10-1" label="Password" counter @click:append="show1 = !show1"></v-text-field>
                         <v-btn class="mr-4" @click="login" color="green">submit</v-btn>
                         <v-btn>clear</v-btn>
@@ -24,11 +30,11 @@
 </template>
 
 <style>
-
 h2 {
     font-weight: normal;
     color: teal;
 }
+
 .v-content__wrap {
     -webkit-box-flex: 1;
     -ms-flex: 1 1 auto;
@@ -38,6 +44,7 @@ h2 {
     background-image: url('http://www.yellowstoneholding.ch/Content/Image/38391858_ML.jpg');
     background-size: cover;
 }
+
 .v-application .text-center {
     text-align: center !important;
     font-size: 80px;
@@ -54,9 +61,13 @@ h2 {
 export default {
     data() {
         return {
+            y: 'top',
+            snackbar: false,
+            text: 'Username or Password is Incorrect! ',
+            timeout: 2000,
             show1: false,
-                username: "",
-                password: "",
+            username: "",
+            password: "",
             rules: {
                 required: value => !!value || "Required.",
                 min: value => value.length >= 8 || "Min 8 characters",
@@ -70,16 +81,19 @@ export default {
                 }
             }
         };
-        
+
     },
     methods: {
-		 	login: function () {
-		   		let username = this.username 
-		   		let password = this.password
-		   		this.$store.dispatch('login', { username, password })
-		   		.then(() => this.$router.push('/employees'))
-		   		.catch(err => window.console.log(err))
-		   	}
-		}
+        login: function () {
+            let username = this.username
+            let password = this.password
+            this.$store.dispatch('login', {
+                    username,
+                    password
+                })
+                .then(() => this.$router.push('/employees'))
+                .catch(() => this.snackbar = true)
+        }
+    }
 };
 </script>
